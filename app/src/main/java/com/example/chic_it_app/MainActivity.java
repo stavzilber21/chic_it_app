b3f26c6b173512ee641ebb2f0463a79fa1a6c74a
hodaya Turgeman <hodaya.tor@gmail.com>
2022-12-27 21:53:20 +0200

    main menu for log out and edit profile

--- app/src/main/java/com/example/chic_it_app/MainActivity.java
+++ app/src/main/java/com/example/chic_it_app/MainActivity.java
@@ -1,14 +1,24 @@
 package com.example.chic_it_app;
-
+//import android.support.v7.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.PopupMenu;
+import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements PopupMenu.OnMenuItemClickListener  {
     Button user, creating_content;
+    ImageView more;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +27,7 @@ public class MainActivity extends AppCompatActivity {
 
         user = (Button) findViewById(R.id.user);
         creating_content = (Button) findViewById(R.id.creating_content);
+        more = (ImageView) findViewById(R.id.more);
         //to connect the buttoms to xml file
         user.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -31,5 +42,53 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(new Intent(MainActivity.this, CreatingcontentActivity.class));
             }
         });
+
+        more.setOnClickListener(new View.OnClickListener()
+        {
+            public void onClick(View v) {
+
+                showPopup(v);
+
+            }
+        });
+
     }
+
+    private void showPopup(View v) {
+        PopupMenu popup = new PopupMenu(this, v);
+        MenuInflater inflater = popup.getMenuInflater();
+        inflater.inflate(R.menu.main_menu, popup.getMenu());
+        popup.show();
+        popup.setOnMenuItemClickListener(MainActivity.this);
+    }
+
+    public void showMenu(View v) {
+        PopupMenu popup = new PopupMenu(this, v);
+
+        // This activity implements OnMenuItemClickListener
+        popup.setOnMenuItemClickListener((PopupMenu.OnMenuItemClickListener) this);
+        popup.inflate(R.menu.main_menu);
+        popup.show();
+    }
+
+   @Override
+    public boolean onMenuItemClick(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.edit_profile:
+                startActivity(new Intent(MainActivity.this , EditProfileActivity.class));
+                break;
+            case R.id.logOut:
+                startActivity(new Intent(MainActivity.this , LoginActivity.class));
+                break;
+            default:
+                return false;
+        }
+        return  true;
+    }
+
+
+
+
+
+
 }
